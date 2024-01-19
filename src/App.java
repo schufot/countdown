1b66fe0f961c249b4f9ec34598b3c0ef688d94ac
schufot <156847189+schufot@users.noreply.github.com>
2024-01-19 13:59:08 +0100

    Add two getDateDiff methods

---
+++ src/App.java
@@ -0,0 +1,31 @@
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        getDateDiffWithUserInput();
+    }
+
+    public static void getDateDiffWithUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the date: ");
+        String date = scanner.next();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-mm-yyyy");
+        Date date2 = null;
+        try {
+            date2 = (Date) dateFormat.parse(date);
+            System.out.print("You have " + ((Temporal) date2).until(LocalDate.now(), ChronoUnit.DAYS) + " days left.");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public long getDateDiff(LocalDate date) {
+        return date.until(LocalDate.now(), ChronoUnit.DAYS);
+    }
+}
