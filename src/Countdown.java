a6b92a8e0730bebe00fb10ad2be23f41df514b95
schufot <156847189+schufot@users.noreply.github.com>
2024-03-12 16:04:41 +0100

    Add ActionListener

--- src/Countdown.java
+++ src/Countdown.java
@@ -1,6 +1,8 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -13,7 +15,9 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Countdown {
     public static void main(String[] args) {
@@ -26,14 +30,36 @@ public class Countdown {
         JButton button = new JButton("Calculate Difference");
         JLabel label = new JLabel("Days until");
         JPanel panel = new JPanel();
+        JTextField startField = new JTextField();
+        JTextField endField = new JTextField();
+        JTextField outputField = new JTextField();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
+        panel.add(startField);
+        panel.add(endField);
         panel.add(button);
+        panel.add(outputField);
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
 
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (formatCheckValidate(startField.getText()) && formatCheckValidate(endField.getText())) {
+                    JOptionPane.showMessageDialog(null, "Valid format", null, JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid format! Input must be in the format 'YYYY-MM-DD'",
+                            null, JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+    }
+
+    public boolean formatCheckValidate(String input) {
+        return input.matches("\\d{4}-\\d{2}-\\d{2}");
     }
 
     public static void getDateDiffWithUserInput() {
